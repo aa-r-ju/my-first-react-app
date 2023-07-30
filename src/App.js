9d80abb5c4acc408edcffb24419b51a62b8766ae
aa-r-ju <aarju3447@gmail.com>
2023-07-30 22:20:19 +0545

    feat: complete workshop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-const App = (props) => {
-  const { notes } = props
-
+const Note = ({ note }) => {
   return (
-    <div>
-      <h1>Notes</h1>
-     <ul>
-     {notes.map((note, i) => 
-    <li key={i}>
-      {note.content}
-    </li>
-  )}
-
-      </ul>
-
-    </div>
+    <li>{note.content}</li>
   )
 }
 
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
 export default App
+
+// [          <Note key={note.id = 1} note={note} />,
+// <Note key={note.id = 2} note={note} />,
+//           <Note key={note.id = 3} note={note} />
+//         ],
+
+//[<li> html is easy<li/>, <li>Browser can execute only JavaScript<li/>, <li> GET and POST are the most important methods of HTTP protocol <li/> ]
